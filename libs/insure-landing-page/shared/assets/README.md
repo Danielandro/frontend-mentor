4995a8a278e5ec539b3169c9dc738ad3f525bb96
Danny <mtme13@hotmail.com>
2020-05-27 12:15:40 +0100

    chore: add design assets for insure landing page

---
+++ libs/insure-landing-page/shared/assets/README.md
@@ -0,0 +1,7 @@
+# insure-landing-page-shared-assets
+
+This library was generated with [Nx](https://nx.dev).
+
+## Running unit tests
+
+Run `ng test insure-landing-page-shared-assets` to execute the unit tests via [Jest](https://jestjs.io).
---
+++ libs/insure-landing-page/shared/assets/jest.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  name: 'insure-landing-page-shared-assets',
+  preset: '../../../../jest.config.js',
+  transform: {
+    '^.+\\.[tj]sx?$': 'ts-jest',
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'html'],
+  coverageDirectory:
+    '../../../../coverage/libs/insure-landing-page/shared/assets',
+};
